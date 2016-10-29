7161f8a3a030fc1e32a66d670899449b980858dc
Phill Conrad <pconrad@cs.ucsb.edu>
2016-10-28 19:40:20 -0700

    new_update_parser remove tokenizer

--- src/edu/ucsb/cs56/pconrad/parsing/tokenizer/AddEqualsToken.java
+++
@@ -1,7 +0,0 @@
-package edu.ucsb.cs56.pconrad.parsing.tokenizer;
-
-public class AddEqualsToken extends Token {
-
-    @Override
-    public String toString() { return "AddEqualsToken"; }
-}
--- src/edu/ucsb/cs56/pconrad/parsing/tokenizer/AddLessThanOrEqualsToken.java
+++
@@ -1,7 +0,0 @@
-package edu.ucsb.cs56.pconrad.parsing.tokenizer;
-
-public class AddLessThanOrEqualsToken extends Token {
-
-    @Override
-    public String toString() { return "AddLessThanOrEqualsToken"; }
-}
