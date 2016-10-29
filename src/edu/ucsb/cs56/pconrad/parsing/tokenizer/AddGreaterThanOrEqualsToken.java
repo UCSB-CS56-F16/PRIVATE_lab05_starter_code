3dc909feac2dd02f642013ea1467863a541e3ecc
Phill Conrad <pconrad@cs.ucsb.edu>
2016-10-28 19:42:52 -0700

    tokenizer

--- src/edu/ucsb/cs56/pconrad/parsing/tokenizer/AddGreaterThanOrEqualsToken.java
+++
@@ -1,7 +0,0 @@
-package edu.ucsb.cs56.pconrad.parsing.tokenizer;
-
-public class AddGreaterThanOrEqualsToken extends Token {
-
-    @Override
-    public String toString() { return "AddGreaterThanOrEqualsToken"; }
-}
--- src/edu/ucsb/cs56/pconrad/parsing/tokenizer/AddNotEqualsToken.java
+++
@@ -1,7 +0,0 @@
-package edu.ucsb.cs56.pconrad.parsing.tokenizer;
-
-public class AddNotEqualsToken extends Token {
-
-    @Override
-    public String toString() { return "AddNotEqualsToken"; }
-}
